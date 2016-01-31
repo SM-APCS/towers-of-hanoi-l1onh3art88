a66ed8050679224c43b1cf21872d6ecf407b2e07
Kurt <Kurt@10.0.1.15>
2016-01-31 13:41:21 -0600

    updating assignment details

---
+++ Hanoi/src/hanoi/Hanoi.java
@@ -0,0 +1,16 @@
+package hanoi;
+
+/**
+ *
+ * @author insert your name here
+ */
+public class Hanoi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
